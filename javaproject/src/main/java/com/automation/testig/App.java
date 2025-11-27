229113bb620043e762384a3d4004edc6e75cb883
AlvaroTarrifaSTE <alvarotarrifa.ste@gmail.com>
2025-11-26 21:53:08 -0500

    This is my first push

--- javaproject/src/main/java/com/automation/testig/App.java
+++ javaproject/src/main/java/com/automation/testig/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello Automation Testing Course!" );
+        System.out.println( "Mi primer push" );
     }
 }
